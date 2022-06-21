7af0b2fdf3b5cefb790a5f0f4cfe24df67103abc
J Phx <zhiyu.wang@ecreditpal.com>
2022-06-21 14:22:39 +0800

    Update follower script

--- TwitterFollowers.py
+++ TwitterFollowers.py
@@ -10,26 +10,31 @@ follow_params = {
 }
 
 
-def getFollowers(_url, _followers):
+def pullRequest(_url):
     response = requests.get(_url, params=follow_params, headers=REQUEST_HEADERS)
+    processResponse(response)
+
+
+def processResponse(response):
+    url = response.request.url
     if response.status_code != 200:
         print(f"ERROR: {response.text}")
         time.sleep(300)
-        getFollowers(_url, _followers)
+        pullRequest(url)
 
     result_json = response.json()
     if result_json.get("data"):
         for user in result_json["data"]:
-            _followers[user["id"]] = user["username"]
+            followers_data[user["id"]] = user["username"]
     else:
-        print("Can't find data")
+        print("Over")
         return
 
-    print(f"The number of fans has been captured <{len(_followers)}>.")
+    print(f"The number of fans has been captured <{len(followers_data)}>.")
     time.sleep(120)
     if result_json["meta"].get("next_token"):
         follow_params["pagination_token"] = result_json["meta"]["next_token"]
-        getFollowers(_url, _followers)
+        pullRequest(url)
 
 
 if __name__ == "__main__":
@@ -38,7 +43,7 @@ if __name__ == "__main__":
     for user_id in REQOIRED_FOLLOW_ID.keys():
         followers_data = {}
         url = f"https://api.twitter.com/2/users/{user_id}/followers"
-        getFollowers(url, followers_data)
+        pullRequest(url)
 
         print("Ready to write files...")
         # write
